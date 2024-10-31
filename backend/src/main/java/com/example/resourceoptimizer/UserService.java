1a0fc5877399a082c1bdb04013a50c9b9c72c21c
Alexandru Nite <alexnite728@gmail.com>
2024-10-31 22:03:28 +0200

    added UserService

---
+++ backend/src/main/java/com/example/resourceoptimizer/UserService.java
@@ -0,0 +1,35 @@
+package com.example.resourceoptimizer;
+
+import com.example.resourceoptimizer.User;
+import com.example.resourceoptimizer.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String id) {
+        userRepository.deleteById(id);
+    }
+
+    public Optional<User> getUserById(String id) {
+        return userRepository.findById(id);
+    }
+}
