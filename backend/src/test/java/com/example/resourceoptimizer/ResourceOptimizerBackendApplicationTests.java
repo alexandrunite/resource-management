29280105b24916c40b52b2efecf6362dbb3e1e3f
Alexandru Nite <alexnite728@gmail.com>
2024-10-31 20:51:54 +0200

    Configured SpringBoot.

---
+++ backend/src/test/java/com/example/resourceoptimizer/ResourceOptimizerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resourceoptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceOptimizerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
